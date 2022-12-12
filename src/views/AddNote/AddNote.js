2479125646020795ee30855aa7f19e966ad7dfb5
Mukesh Pimpalkar <pimpalkarmukesh07@gmail.com>
2022-12-12 13:59:11 +0530

    final commit

---
+++ src/views/AddNote/AddNote.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './AddNote.css'
+const AddNote = () => {
+  return (
+    <>
+      <div>
+        <h2>This is AddNote</h2>
+      </div>
+    </>
+  )
+}
+
+export default AddNote
